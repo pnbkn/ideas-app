14cb3e11e9d9277ad3204bc4297f6c5e7ee72d35
pnbkn <pnblackburn@gmail.com>
2020-12-15 14:53:07 -0800

    Initial Commit

---
+++ Thoughts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thoughts
+//
+//  Created by Paul Blackburn on 12/15/20.
+//  Copyright © 2020 Paul Blackburn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
